a048ba9ef7c9f1a1187834d536aa18b3a09b2985
Bryant <bryly27@sbcglobal.net>
2015-05-22 16:08:42 -0700

    fixed errors

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
 ?>
 
 <?xml version="1.0" encoding="UTF-8"?>;
-
+<response>
 <?php  
 	
 			$user_pushed = (int) $_REQUEST['Digits'];
@@ -25,3 +25,4 @@
 				<say>please enter a valid number</say>
 				<redirect>test.php</redirect>
 <?php	} ?>
+</response>
--- test.php
+++ test.php
@@ -1,16 +1,12 @@
-<?php  
-	
-	header('Content-type: text/xml');
-?>
 <?xml version="1.0" encoding="UTF-8"?>;
-	<Response>
-		<Gather action='process.php' numDigits='1'>
-			<Say>Welcome to Bryant's test Number</Say>
-			<Say>If you are cool, press 1</Say>
-			<Say>If you are hot, press 2</Say>
-			<Say>If you are warm, press 3</Say>
-		</Gather>
+<Response>
+	<Gather action='process.php' numDigits='1'>
+		<Say>Welcome to Bryant's test Number</Say>
+		<Say>If you are cool, press 1</Say>
+		<Say>If you are hot, press 2</Say>
+		<Say>If you are warm, press 3</Say>
+	</Gather>
 
-		<Say>Sorry, I didn't get your response</Say>
-		<redirect>test.php</redirect>
-	</Response>
+	<Say>Sorry, I didn't get your response</Say>
+	<redirect>test.php</redirect>
+</Response>
